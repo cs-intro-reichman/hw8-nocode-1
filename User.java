0c03af2fc3590614ddaf32dd86f8757038642522
Nathan <nathan.orlowski@post.runi.ac.il>
2025-01-09 10:21:19 +0200

    0

--- User.java
+++ User.java
@@ -53,16 +53,18 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        for (int i = 0; i < fCount; i++){
-        if (follows[i].equals(name) || fCount == maxfCount) {
+        String lowerCaseName = name.toLowerCase();
+    for (int i = 0; i < fCount; i++) {
+        if (follows[i].toLowerCase().equals(lowerCaseName) || fCount == maxfCount) {
             return false;
         }
     }
-
     follows[fCount] = name;
     fCount++;
     return true;
     }
+
+   
 
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
